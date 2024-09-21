0069d9b5d2aaac136679ed03dd8c3f291c1577bd
Dylan-Cantu <dcantu208@student.alamo.edu>
2024-09-20 21:37:58 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,44 @@
+const age = 20;
+
+if(age > 18){
+    console.log("You are good to go!");
+} else {
+    console.log("you are not old enough")
+};
+
+const name = "Dylan";
+
+const course = "IMED-2351";
+
+console.log("Hello my name is " + name + " and this class is " + course);
+
+
+let txt =
+ 'This is a string method in JavaScript'
+
+ let change = 100 - 65.47 + " This is a number method." +
+ " Number methods are good when dealing with money."
+
+ let day;
+switch (new Date().getDay()) {
+  case 0:
+    day = "Sunday";
+    break;
+  case 1:
+    day = "Monday";
+    break;
+  case 2:
+    day = "Tuesday";
+    break;
+  case 3:
+    day = "Wednesday";
+    break;
+  case 4:
+    day = "Thursday";
+    break;
+  case 5:
+    day = "Friday";
+    break;
+  case  6:
+    day = "Saturday";
+}
